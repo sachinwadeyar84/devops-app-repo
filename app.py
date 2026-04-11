8ac585e08a632a9050ce81d89c247745430de63e
sachinwadeyar84 <sachinwadeyar84@gmail.com>
2026-04-12 00:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from flask import Flask
-from prometheus_client import Counter, generate_latest
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
-# 🔥 Create metric
+# Create metric
 REQUEST_COUNT = Counter('app_requests_total', 'Total number of requests')
 
 @app.route("/")
 def home():
-    REQUEST_COUNT.inc()   # count requests
+    REQUEST_COUNT.inc()
     return "Hello from DevOps + Kubernetes 🚀"
 
 @app.route("/health")
 def health():
     return "OK"
 
-# 🔥 IMPORTANT: Prometheus endpoint
+# 🔥 IMPORTANT: metrics endpoint
 @app.route("/metrics")
 def metrics():
-    return generate_latest(), 200, {'Content-Type': 'text/plain'}
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
